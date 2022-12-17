9ccee18e864e373abc8df852f67e78fd984c8a9a
Anatolii Vasilev <avas2012@gmail.com>
2022-12-17 23:39:07 +0300

    kafka added controller method sendMsg

--- src/main/java/com/mycleancode/kafkaspring/controller/MsgController.java
+++ src/main/java/com/mycleancode/kafkaspring/controller/MsgController.java
@@ -2,6 +2,8 @@ package com.mycleancode.kafkaspring.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.SendResult;
+import org.springframework.util.concurrent.ListenableFuture;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,8 +15,15 @@ public class MsgController {
     @Autowired
     private KafkaTemplate<String, String> kafkaTemplate;
 
+//    @PostMapping
+//    public void sendOrder(String msgId, String msg) {
+//        kafkaTemplate.send("msg", msgId, msg);
+//    }
+
     @PostMapping
-    public void sendOrder(String msgId, String msg) {
-        kafkaTemplate.send("msg", msgId, msg);
+    public void sendMsg(String msgId, String msg) {
+        ListenableFuture<SendResult<String, String>> future = kafkaTemplate.send("msg", msgId, msg);
+        future.addCallback(System.out::println, System.err::println);
+        kafkaTemplate.flush();
     }
 }
